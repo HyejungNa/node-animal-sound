27f742811ed9d9cb656423592abd8ab87cd9b2cf
Hyejung Na <hyejung.na01@gmail.com>
2022-09-05 16:18:25 +1000

    first-commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+var cors = require('cors')
+const app = express()
+const port = 3000
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/sound/:name', (req, res) => {
+    const { name } = req.params
+
+    if (name == "dog") {
+        res.json({'sound' : 'woof'})
+    } else if(name == "cat") {
+        res.json({'sound' : 'meow'})
+    } else if(name== "pig") {
+        res.json({'sound' : 'oink'})
+    } else {
+        res.json({'sound' : 'unknown'})
+    }
+})
+
+app.listen(3000)
